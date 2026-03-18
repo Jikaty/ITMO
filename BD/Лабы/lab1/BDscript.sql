ae2e4c5f76b3fea8ceb28ea9fb5211378c9ff253
Kiselev Timofei <jikaty@niuitmo.ru>
2026-03-18 15:51:32 +0300

    update DB lab1

--- BD/Лабы/lab1/BDscript.sql
+++ BD/Лабы/lab1/BDscript.sql
@@ -1,8 +1,23 @@
 
-CREATE TYPE object_type AS ENUM ('animal', 'stuff');
+DROP TABLE IF EXISTS sound CASCADE;
+DROP TABLE IF EXISTS emotions CASCADE;
+DROP TABLE IF EXISTS actions CASCADE;
+DROP TABLE IF EXISTS flock CASCADE;
+DROP TABLE IF EXISTS stuff CASCADE;
+DROP TABLE IF EXISTS animals CASCADE;
+DROP TABLE IF EXISTS time_of_day CASCADE;
+DROP TABLE IF EXISTS illuminations CASCADE;
+DROP TABLE IF EXISTS locations CASCADE;
+DROP TABLE IF EXISTS animals_flock CASCADE;
+DROP TYPE IF EXISTS object_type CASCADE;
+DROP TYPE IF EXISTS sex CASCADE;
+DROP TYPE IF EXISTS action_enum CASCADE;
+DROP TYPE IF EXISTS role;
+
+
 CREATE TYPE sex AS ENUM ('М', 'Ж');
 CREATE TYPE action_enum AS ENUM ('вести стаю к водопою', 'курить сижки');
-
+CREATE TYPE role AS ENUM ('Вожак', 'Не вожак');
 
 CREATE TABLE locations (
                            id SERIAL PRIMARY KEY,
@@ -17,16 +32,19 @@ CREATE TABLE illuminations (
                                source VARCHAR(50)
 );
 
-CREATE TABLE objects (
-                         id SERIAL PRIMARY KEY,
-                         type object_type NOT NULL
-);
-
 
 CREATE TABLE time_of_day (
                              id SERIAL PRIMARY KEY,
                              name VARCHAR(50),
-                             illumination_id INTEGER REFERENCES illuminations(id)
+                             illumination_id INTEGER REFERENCES illuminations(id),
+                             start_time TIME NOT NULL,
+                             end_time TIME NOT NULL
+);
+
+CREATE TABLE emotions (
+                          id SERIAL PRIMARY KEY,
+                          name VARCHAR(50),
+                          spawn_time_id INTEGER REFERENCES time_of_day(id) NOT NULL
 );
 
 CREATE TABLE animals (
@@ -34,8 +52,8 @@ CREATE TABLE animals (
                          name VARCHAR(50),
                          sex sex NOT NULL ,
                          emotion_id INTEGER REFERENCES emotions(id),
-                         current_location_id INTEGER REFERENCES locations(id) NOT NULL ,
-                         object_id INTEGER REFERENCES objects(id) NOT NULL
+                         current_location_id INTEGER REFERENCES locations(id) NOT NULL
+
 );
 
 CREATE TABLE stuff (
@@ -43,15 +61,20 @@ CREATE TABLE stuff (
                        name VARCHAR(100),
                        property VARCHAR(50) NOT NULL,
                        material VARCHAR(50),
-                       current_location_id INTEGER REFERENCES locations(id) NOT NULL ,
-                       object_id INTEGER REFERENCES objects(id) NOT NULL
+                       current_location_id INTEGER REFERENCES locations(id) NOT NULL
 );
 
 
 CREATE TABLE flock (
                        id SERIAL PRIMARY KEY,
-                       quantity INTEGER,
-                       leader_id INTEGER REFERENCES animals(id) NOT NULL
+                       name VARCHAR(50)
+);
+
+CREATE TABLE animals_flock(
+                              id SERIAL PRIMARY KEY,
+                              flock_id INTEGER REFERENCES flock(id) NOT NULL,
+                              animal_id INTEGER REFERENCES animals(id) NOT NULL ,
+                              role role NOT NULL
 );
 
 CREATE TABLE actions (
@@ -59,13 +82,6 @@ CREATE TABLE actions (
                          action_val action_enum NOT NULL ,
                          stuff_id INTEGER REFERENCES stuff(id),
                          subject_id INTEGER REFERENCES animals(id) NOT NULL
-);
-
-CREATE TABLE emotions (
-                          id SERIAL PRIMARY KEY,
-                          name VARCHAR(50),
-                          subject_id INTEGER REFERENCES animals(id) NOT NULL ,
-                          spawn_time_id INTEGER REFERENCES time_of_day(id) NOT NULL
 );
 
 CREATE TABLE sound (
@@ -78,24 +94,27 @@ CREATE TABLE sound (
 
 INSERT INTO locations (name, coordinates) VALUES ('Водопой', POINT(0,0));
 INSERT INTO illuminations (type, illumination_power, source) VALUES ('слабый', 10, 'заря');
-INSERT INTO time_of_day (name, illumination_id) VALUES ('нарождающийся день', 1);
+INSERT INTO time_of_day (name, illumination_id, start_time, end_time) VALUES ('нарождающийся день', 1, '20:00','22:00');
+INSERT INTO emotions (name, spawn_time_id) VALUES ('спокойствие', 1);
 
 
-INSERT INTO objects (type) VALUES ('animal');
-INSERT INTO animals (name, sex, current_location_id, object_id) VALUES ('Смотрящий', 'М', 1, 1);
 
 
-INSERT INTO objects (type) VALUES ('stuff');
-INSERT INTO stuff (name, property, material, current_location_id, object_id)
-VALUES ('Новый Камень', 'странный/необычайный', 'камень', 1, 2);
+INSERT INTO animals (name, sex, emotion_id,current_location_id) VALUES ('Смотрящий', 'М', 1,1);
+INSERT INTO flock(name) VALUES ('Воронины');
+INSERT INTO animals_flock(flock_id, animal_id, role) VALUES (1,1,'Вожак');
+
+
+INSERT INTO stuff (name, property, material, current_location_id)
+VALUES ('Новый Камень', 'странный/необычайный', 'камень', 1);
 
 
 INSERT INTO actions (action_val, stuff_id, subject_id) VALUES ('вести стаю к водопою', 1, 1);
-INSERT INTO emotions (name, subject_id, spawn_time_id) VALUES ('спокойствие', 1, 1);
+
 
 
 INSERT INTO sound (property, source, spawn_time_id) VALUES ('необычайный', 'собака', 1);
-COMMIT;
+
 
 
 
